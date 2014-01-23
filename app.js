1a7342d7eb8d25a565665d7cf286b3b6fade1234
naitse <sebastian.gramano@mulesorce.com>
2014-01-23 19:09:56 -0300

    its alive

--- app.js
+++ app.js
@@ -25,8 +25,9 @@ app.use(express.static(__dirname + '/static'));
 function getSeconds(time)
 {
     var today = new Date()
+    time = time.replace('.', ':');
     var ts = time.split(':');
-    return Date.UTC((today.getUTCFullYear()), (today.getUTCMonth()), (today.getUTCDate()), ts[0], ts[1], ts[2]);
+    return Date.UTC((today.getUTCFullYear()), (today.getUTCMonth()), (today.getUTCDate()), ts[0], ts[1], ts[2], ts[3]);
 }
 
 app.get('/api/latency', function (req, res) {
@@ -49,31 +50,79 @@ app.get('/api/latency', function (req, res) {
 
 });
 
+// app.get('/api/responseTime', function (req, res) {
+// 	var output = [];
+
+// 	var parser = new xml2js.Parser();
+
+// 	fs.readFile(__dirname + '/responsetime.xml', function(err, data) {
+//     	parser.parseString(data, function (err, parsedObject) {
+
+//     		_.each(parsedObject.testResults.httpSample, function(sample){
+//     			sample = sample.$
+//     			output.push([parseInt(sample.ts), parseInt(sample.t)])
+
+//     		})
+
+//        	 res.json(output);
+//     	});
+// 	});
+
+// });
 app.get('/api/responseTime', function (req, res) {
-	var output = [];
+    var output = [];
 
-	var parser = new xml2js.Parser();
+    command = "cd /Applications/apache-jmeter-2.10/lib/ext/ && java -jar CMDRunner.jar --tool Reporter --generate-csv /temp/rt.csv --input-jtl /temp/results.jtl --plugin-type ResponseTimesOverTime"
+    exec(command, {maxBuffer: 5024*1024}, function(error, stdout, stderr){
+      if (error !== null) {
+          console.log('exec error: ' + error);
+          res.json(false);
+      }
 
-	fs.readFile(__dirname + '/responsetime.xml', function(err, data) {
-    	parser.parseString(data, function (err, parsedObject) {
 
-    		_.each(parsedObject.testResults.httpSample, function(sample){
-    			sample = sample.$
-    			output.push([parseInt(sample.ts), parseInt(sample.t)])
+      //CSV File Path or CSV String or Readable Stream Object
+        var csvFileName="/temp/rt.csv";
 
-    		})
+        //new converter instance
+        var csvConverter=new Converter();
 
-       	 res.json(output);
-    	});
-	});
+        //end_parsed will be emitted once parsing finished
+        csvConverter.on("end_parsed",function(jsonObj){
+            _.each(jsonObj.csvRows, function(row){
+                output.push([new Date(getSeconds(row['Elapsed time'])).getTime(), parseInt(row['HTTP Request'])])
+            })
+
+            res.json(output);
+
+        });
+
+        csvConverter.from(csvFileName);
+
+
+    });
+
+    // var parser = new xml2js.Parser();
+
+    // fs.readFile(__dirname + '/hps.xml', function(err, data) {
+ //     parser.parseString(data, function (err, parsedObject) {
+
+ //         _.each(parsedObject.testResults.httpSample, function(sample){
+ //             sample = sample.$
+ //             output.push([parseInt(sample.ts), parseInt(sample.t)])
+
+ //         })
+
+ //          res.json(output);
+ //     });
+    // });
 
 });
 
 app.get('/api/hps', function (req, res) {
 	var output = [];
 
-    command = "cd /Applications/apache-jmeter-2.10/lib/ext/ && java -jar CMDRunner.jar --tool Reporter --generate-csv /Users/sebastiangramano/hps.csv --input-jtl /Users/sebastiangramano/results.jtl --plugin-type HitsPerSecond"
-    exec(command, function(error, stdout, stderr){
+    command = "cd /Applications/apache-jmeter-2.10/lib/ext/ && java -jar CMDRunner.jar --tool Reporter --generate-csv /temp/hps.csv --input-jtl /temp/results.jtl --plugin-type HitsPerSecond"
+    exec(command, {maxBuffer: 5024*1024}, function(error, stdout, stderr){
       if (error !== null) {
           console.log('exec error: ' + error);
           res.end();
@@ -81,7 +130,7 @@ app.get('/api/hps', function (req, res) {
 
 
       //CSV File Path or CSV String or Readable Stream Object
-        var csvFileName="/Users/sebastiangramano/hps.csv";
+        var csvFileName="/temp/hps.csv";
 
         //new converter instance
         var csvConverter=new Converter();
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -3,55 +3,134 @@
 /* Controllers */
 
 angular.module('nmeter.controllers', []).
-  controller('LatencyCtrl', ['$scope', 'webapi', 'charttheme', function($scope, webapi, charttheme) {
+  controller('LatencyCtrl', ['$scope', 'webapi', 'charttheme', '$q', function($scope, webapi, charttheme, $q) {
 
-		var chart
   		var highchartsOptions = Highcharts.setOptions(charttheme);
   		var colors = Highcharts.getOptions().colors;
 
-		$scope.$on('$viewContentLoaded', function() {
-			chart = new Highcharts.Chart({
-		        chart: {
-		            renderTo: 'latency',
-		            defaultSeriesType: 'spline',
-		            events: {
-		                load: requestData
-		            }
-		        },
-		        title: {
-		            text: 'Live random data'
-		        },
-		        xAxis: {
-		            type: 'datetime',
-		            tickPixelInterval: 150,
-		            maxZoom: 20 * 1000
-		        },
-		        yAxis: {
-		            minPadding: 0.2,
-		            maxPadding: 0.2,
-		            title: {
-		                text: 'Value',
-		                margin: 80
-		            }
-		        },
-		        series: [{
-		            name: 'HPS',
-		            data: []
-		        }]
-		    });     
+  		var rtchart;
+  		var hpschart;
+
+		// hpschart = new Highcharts.Chart({
+		//         chart: {
+		//             renderTo: 'hps',
+		//             events: {
+		//                 load: requestData
+		//             }
+		//         },
+		//         title: {
+		//             text: ''
+		//         },
+		//         xAxis: {
+		//             type: 'datetime'
+		//         },
+		//         yAxis: {
+		//             minPadding: 0.2,
+		//             maxPadding: 0.2,
+		//             title: {
+		//                 text: 'Value',
+		//                 margin: 80
+		//             }
+		//         },
+		//         series: [{
+		//             name: 'HPS',
+		//             data: []
+		//         }]
+		//     });     
+
+		// function requestData() {
+
+	 //    	var deferreds = new Array()
 
 
-			requestData()
-		})
+  //   		deferreds['hps'] = $q.defer();
+		// 	webapi.getHPS(function(data){
 
-		function requestData() {
-			webapi.getHPS(function(data){
 
-				chart.series[0].setData(data, true);
-				setTimeout(requestData, 5000);
+		// 		hpschart.series[0].setData(data, true);
+		// 		deferreds['hps'].resolve(data);
 
+		// 	});
+			
+		    
+		//     $q.all(deferreds).then(function(val){
+		// 		setTimeout(requestData, 5000);
+		//     })
+		// }
+
+		var hpsData = []
+
+		hpschart = new Highcharts.StockChart({
+			chart:{
+				renderTo: 'hps',
+				animation:false
+			},
+			rangeSelector : {
+				enabled : false,
+				inputEnabled: false
+			},
+
+			title : {
+				text : 'Hits per secons'
+			},
+			
+			series : [{
+		            name: 'Hits/s',
+		            data: hpsData
+		     }]
+		}); 
+
+		var rtData = []
+
+		rtchart = new Highcharts.StockChart({
+			chart:{
+				renderTo: 'restime',
+				animation:false
+			},
+			rangeSelector : {
+				enabled : false,
+				inputEnabled: false
+			},
+
+			title : {
+				text : 'Response Time'
+			},
+			
+			series : [{
+		            name: 'ms',
+		            data: rtData
+		     }]
+		});  
+
+		requestRTData()
+		requestHPSData()
+
+		function requestRTData() {
+
+	    	var deferreds = new Array()
+
+    		deferreds['RT'] = $q.defer();
+			webapi.getResponseTime(function(data){
+				rtchart.series[0].setData(data, true);
+				setTimeout(requestRTData(), 5000);
 			});
+		    
 		}
+
+		function requestHPSData() {
+
+	    	var deferreds = new Array()
+
+    		deferreds['RT'] = $q.defer();
+			webapi.getHPS(function(data){
+				hpschart.series[0].setData(data, true);
+				setTimeout(requestHPSData(), 5000);
+			});
+		    
+		}
+
+
+
 
 		
 
